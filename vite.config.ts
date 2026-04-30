2e45ee079fdd47768c0a1e8e240017772ae5f6b9
Jack Shelton <me@jackshelton.com>
2026-04-30 16:55:10 -0500

    init

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig(({ command, mode }): UserConfig => {
     plugins: [
       qwikRouter(),
       qwikVite({
-        experimental: ["suspense"],
+        experimental: ["suspense", "noSPA"],
         devTools: {
           hmr: false,
         },
